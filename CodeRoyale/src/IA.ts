59fec9ff54e1e344db3ce7147fc81240af0c3656
descampsk <kevin.descamps@tinyclues.com>
2022-12-08 15:29:00 +0100

    [CodeRoyale][bronze][12] remove some useless stuff

--- CodeRoyale/src/IA.ts
+++ CodeRoyale/src/IA.ts
@@ -31,22 +31,6 @@ export class IA {
     }
   }
 
-  checkRush() {
-    const { nearestSites } = this.state;
-    const maxMineSize = nearestSites
-      .slice(0, 2)
-      .reduce((income, site) => income + site.maxMineSize, 0);
-
-    if (
-      this.state.turn === 1 &&
-      this.state.queen.health < 100 &&
-      maxMineSize >= 4
-    ) {
-      this.isRushable = true;
-    }
-    console.error("isRushable", this.isRushable);
-  }
-
   checkDangerQueen(dangerRadius = 1000) {
     const { queen, ennemyKnights, ennemyKnightBarracks } = this.state;
     for (const barrack of ennemyKnightBarracks) {
@@ -69,199 +53,19 @@ export class IA {
     return false;
   }
 
-  doRush() {
-    const {
-      nearestSites,
-      nearestSitesEmpty,
-      myMines,
-      myNearestMines,
-      queen,
-      myKnightBarracks,
-      gold,
-      myIncome,
-      mySites,
-      myTowers,
-    } = this.state;
-
-    if (!this.rushTrained) {
-      const { id } = nearestSitesEmpty.filter(
-        (site) =>
-          (this.side === Side.DOWN && site.position.y > 550) ||
-          (this.side === Side.UP && site.position.y < 450)
-      )[0];
+  isMinesOnTheWay(destination: Point) {
+    const { myMines, queen } = this.state;
+    const line = new Line(destination, queen.position);
+    for (const mine of myMines) {
+      console.error(mine.id, line.distanceSquared(mine.position));
       if (
-        myNearestMines.length &&
-        myNearestMines[0].cooldown < myNearestMines[0].maxMineSize &&
-        myIncome < 5
+        Math.sqrt(line.distanceSquared(mine.position)) < 100 &&
+        computeSiteDistance(mine, queen) < 300
       ) {
-        console.log(`BUILD ${myNearestMines[0].id} MINE`);
-      } else if (myMines.length < 2 && myIncome < 4) {
-        console.log(`BUILD ${id} MINE`);
-      } else if (myKnightBarracks.length < 2) {
-        console.log(`BUILD ${id} BARRACKS-KNIGHT`);
-      } else {
-        console.log(`BUILD ${id} TOWER`);
-      }
-    } else {
-      const myNearestSites = nearestSites.filter(
-        (site) =>
-          site.owner !== Owner.ALLY && site.structure !== Structure.TOWER
-      );
-      console.log(`BUILD ${myNearestSites[0].id} TOWER`);
-    }
-
-    if (gold >= 160 && myKnightBarracks.length >= 2) {
-      this.rushTrained = true;
-      console.log(`TRAIN ${myKnightBarracks.map((site) => site.id).join(" ")}`);
-    } else {
-      console.log("TRAIN");
-    }
-  }
-
-  doAction() {
-    const {
-      sites,
-      queen,
-      gold,
-      turn,
-      mySites,
-      myMines,
-      myArcherBarracks,
-      myKnightBarracks,
-      myGiantBarracks,
-      ennemyKnightBarracks,
-      ennemyArcherBarracks,
-      ennemyTowers,
-      ennemyKnights,
-      myArchers,
-      myGiants,
-    } = this.state;
-    const queenInDanger = this.checkDangerQueen();
-    const nearestSites = Object.values(sites).sort(
-      (siteA, siteB) =>
-        Point.distance(siteA.position, queen.position) -
-        Point.distance(siteB.position, queen.position)
-    );
-
-    let nearestSitesEmpty = nearestSites.filter(
-      (site) => site.owner !== Owner.ALLY && site.structure !== Structure.TOWER
-    );
-
-    if (ennemyTowers.length >= 3 || queenInDanger) {
-      nearestSitesEmpty = nearestSitesEmpty.filter(
-        (site) =>
-          (this.side === Side.UP && site.position.x <= 1920 / 2) ||
-          (this.side === Side.DOWN && site.position.x >= 1920 / 2)
-      );
-    }
-
-    const nearestSitesEmptyWithAlliesTower = nearestSites.filter(
-      (site) =>
-        (site.owner === Owner.ALLY && site.structure === Structure.TOWER) ||
-        (site.owner === Owner.ENNEMY && site.structure !== Structure.TOWER) ||
-        site.owner === Owner.NONE
-    );
-    const nearestMines = nearestSites.filter(
-      (site) =>
-        site.owner === Owner.ALLY &&
-        site.structure === Structure.MINE &&
-        Point.distance(site.position, queen.position) < 300
-    );
-
-    const nearestTowers = nearestSites.filter(
-      (site) => site.owner === Owner.ALLY && site.structure === Structure.TOWER
-    );
-
-    const siteToBuild =
-      nearestSitesEmptyWithAlliesTower[0] ?? nearestSitesEmpty[0];
-
-    if (!siteToBuild) {
-      console.error(
-        "nearestSitesEmptyWithAlliesTower",
-        nearestSitesEmptyWithAlliesTower
-      );
-      console.error("nearestSitesEmpty", nearestSitesEmpty);
-
-      console.log("WAIT");
-    } else if (queenInDanger) {
-      const nearestSiteEmpty = nearestSitesEmpty[0];
-      const nearestTower = nearestTowers.filter((tower) => tower.unit < 300)[0];
-      if (
-        nearestTower &&
-        Point.distance(nearestTower.position, queen.position) <
-          Point.distance(nearestSiteEmpty.position, queen.position)
-      ) {
-        console.log(`BUILD ${nearestTower.id} TOWER`);
-      } else {
-        console.log(`BUILD ${nearestSiteEmpty.id} TOWER`);
-      }
-    } else if (
-      nearestMines.length &&
-      nearestMines[0].cooldown < nearestMines[0].maxMineSize
-    ) {
-      console.log(`BUILD ${nearestMines[0].id} MINE`);
-    } else if (ennemyKnightBarracks.length - myArcherBarracks.length > 1) {
-      console.log(`BUILD ${siteToBuild.id} BARRACKS-ARCHER`);
-    } else if (!myKnightBarracks.length || gold >= 300) {
-      console.log(`BUILD ${siteToBuild.id} BARRACKS-KNIGHT`);
-    } else if (ennemyTowers.length >= 2 && !myGiantBarracks.length) {
-      console.log(`BUILD ${siteToBuild.id} BARRACKS-GIANT`);
-    } else if (siteToBuild.maxMineSize >= 3 && siteToBuild.gold > 0) {
-      console.log(`BUILD ${siteToBuild.id} MINE`);
-    } else if (myArcherBarracks.length === 0) {
-      console.log(`BUILD ${siteToBuild.id} BARRACKS-ARCHER`);
-    } else if (siteToBuild.gold > 0) {
-      console.log(`BUILD ${siteToBuild.id} MINE`);
-    } else {
-      console.log(`BUILD ${siteToBuild.id} TOWER`);
-    }
-
-    const barracksSiteIds = [];
-
-    const giantSites = myGiantBarracks.map((site) => site.id);
-    let possibleGiant = 0;
-    let waitForGiant = false;
-    if ((ennemyTowers.length - myGiants.length) / 2 > 1) {
-      possibleGiant = Math.floor(this.state.gold / 140);
-      if (!possibleGiant) waitForGiant = true;
-      for (let i = 0; i < possibleGiant; i++) {
-        barracksSiteIds.push(giantSites[i]);
+        return true;
       }
     }
-
-    let possibleArchers = 0;
-    let waitForArcher = false;
-    const archerSiteIds = myArcherBarracks.map((site) => site.id);
-    if (
-      !waitForGiant &&
-      archerSiteIds.length &&
-      (ennemyKnights.length > myArchers.length ||
-        (ennemyKnightBarracks.length >= 2 && myArchers.length <= 2))
-    ) {
-      possibleArchers = Math.floor((gold - possibleGiant * 140) / 100);
-      if (!possibleArchers) waitForArcher = true;
-      for (let i = 0; i < possibleArchers; i++) {
-        barracksSiteIds.push(archerSiteIds[i]);
-      }
-    }
-    const knightSiteIds = myKnightBarracks.map((site) => site.id);
-    if (!waitForGiant && !waitForArcher && knightSiteIds.length) {
-      const possibleKnights = Math.floor(
-        (gold - 100 * possibleArchers - 140 * possibleGiant) / 80
-      );
-      for (let i = 0; i < possibleKnights; i++) {
-        barracksSiteIds.push(knightSiteIds[i]);
-      }
-    }
-    const definedBarracksSiteIds = barracksSiteIds.filter(
-      (site) => site !== undefined
-    );
-    console.error("siteIds", barracksSiteIds);
-    if (definedBarracksSiteIds.length) {
-      console.log(`TRAIN ${definedBarracksSiteIds.join(" ")}`);
-    } else {
-      console.log("TRAIN");
-    }
+    return false;
   }
 
   canTowerShoot(tower: Site) {
@@ -417,8 +221,6 @@ export class IA {
       myTowers,
       ennemyTowers,
       turn,
-      queen,
-      myIncome,
     } = this.state;
     const possibleMines = nearestSites
       .filter(
@@ -449,11 +251,7 @@ export class IA {
         return true;
       });
 
-    if (myIncome >= 9 && (myTowers.length < 1 || myTowers[0].cooldown < 600)) {
-      console.log(
-        `BUILD ${myTowers.length ? myTowers[0].id : possibleMines[0].id} TOWER`
-      );
-    } else if (
+    if (
       myNearestMines.length &&
       myNearestMines[0].cooldown < myNearestMines[0].maxMineSize
     ) {
@@ -528,7 +326,6 @@ export class IA {
       queen,
       myMines,
       myNearestMines,
-      myGiantBarracks,
       myKnightBarracks,
     } = this.state;
     const possibleMines = nearestSitesEmpty.filter((site) => site.gold > 0);
